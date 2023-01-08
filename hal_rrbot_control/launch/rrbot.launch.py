a26211800ae71e8a4dfc2d21749a44490280b0b4
the-snowwhite <producer@holotronic.dk>
2023-01-08 12:20:42 +0100

    fix spanner executable name
    Signed-off-by: the-snowwhite <producer@holotronic.dk>

--- hal_rrbot_control/launch/rrbot.launch.py
+++ hal_rrbot_control/launch/rrbot.launch.py
@@ -286,7 +286,7 @@ def generate_launch_description():
 
     joint_state_broadcaster_spawner = Node(
         package="controller_manager",
-        executable="spawner.py",
+        executable="spawner",
         arguments=[
             "joint_state_broadcaster",
             "--controller-manager",
@@ -296,7 +296,7 @@ def generate_launch_description():
 
     robot_controller_spawner = Node(
         package="controller_manager",
-        executable="spawner.py",
+        executable="spawner",
         arguments=[
             LaunchConfiguration("robot_controller"),
             "-c",
